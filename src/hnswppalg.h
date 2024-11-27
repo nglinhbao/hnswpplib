d4bc8ba47c5005b90b5b6fa5ff50e317fc0a4791
Hy Nguyen <nhy@a100-4.ai.deakin.edu.au>
2024-11-27 12:54:37 +1100

    fix exclude

--- src/hnswppalg.h
+++ src/hnswppalg.h
@@ -23,7 +23,8 @@ public:
         , max_elements_(max_elements)
         , M_(M)
         , ef_construction_(ef_construction)
-        , max_level_(std::floor((1 / std::log(M/2)) * std::log(max_elements/2)))
+        // , max_level_(std::floor((1 / std::log(M/2)) * std::log(max_elements/2)))
+        , max_level_(max_level)
         , scale_factor_(1 / log(1.0 * M_))
         , space_(new hnswlib::L2Space(dim)) {
         
